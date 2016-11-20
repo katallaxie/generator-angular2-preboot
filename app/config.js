8ae1e42ff9fd9b00caa65ee6b02cb8eab3f6ba79
Sebastian Döll <sebastian@pixelmilk.com>
2016-11-20 19:53:05 +0100

    ~ removing dist

--- app/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    tar: 'https://github.com/katallaxie/angular2-preboot/archive/master.tar.gz',
-    npm: {
-        loglevel: 'silent',
-        progress: false,
-    },
-};
-//# sourceMappingURL=config.js.map
